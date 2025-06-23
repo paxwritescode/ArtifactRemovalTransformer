3932e29d3bee097178f2d7112819c756da0c6ed7
paxwritescode <lisadobretsova4@gmail.com>
2025-06-23 13:52:04 +0300

    remove EEG channel, launch ART on our data

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import os
 
 if __name__ == '__main__':
     # parameter setting
-    input_path = './sampledata/'
-    input_name = 'sampledata.csv'
-    sample_rate = 256 # input data sample rate
+    input_path = './data/'
+    input_name = 'eeg_test_converted.csv'
+    sample_rate = 500 # input data sample rate
     modelname = 'ART' # or 'ICUNet', 'ICUNet++', 'ICUNet_attn', 'ART'
-    output_path = './sampledata/'
-    output_name = 'outputsample.csv'
+    output_path = './data/'
+    output_name = 'our_output.csv'
 
     # read the mapping result
-    mapping_name = './sampledata/sample_chanlocs_mapping_result.json'
+    mapping_name = './data/mapped_output.json'
     mapping_result, num_channel, num_group = utils.read_mapping_result(mapping_name)
 
     for i in range(num_group):
