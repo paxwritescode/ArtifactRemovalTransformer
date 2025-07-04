5ed8b553a1b1c2ab2cd2e51a487a6830f139908d
paxwritescode <lisadobretsova4@gmail.com>
2025-07-04 08:50:26 +0300

    add plot by channels function

--- main.py
+++ main.py
@@ -1,24 +1,51 @@
 import utils
 import os
 
+from dataclasses import dataclass
+
+@dataclass
+class InputConfig:
+    input_path: str
+    input_name: str
+    sample_rate: int
+    output_path: str
+    output_name: str
+    mapping_result: str
+
+
 if __name__ == '__main__':
     # parameter setting
-    input_path = './data/'
-    input_name = 'eeg_test_converted.csv'
-    sample_rate = 500 # input data sample rate
-    modelname = 'ART' # or 'ICUNet', 'ICUNet++', 'ICUNet_attn', 'ART'
-    output_path = './data/'
-    output_name = 'our_output.csv'
+    our_input = False
+    ic: InputConfig
+    if our_input:
+        ic = InputConfig(
+            input_path = './data/',
+            input_name = 'eeg_test_converted.csv',
+            sample_rate = 500, # input data sample rate
+            output_path = './data/',
+            output_name = 'our_output.csv',
+            mapping_result = './data/mapped_output.json'
+        )
+    else:
+        ic = InputConfig(
+            input_path = './sampledata/',
+            input_name = 'sampledata.csv',
+            sample_rate = 256,
+            output_path = './sampledata/',
+            output_name ='outputsample.csv',
+            mapping_result = './sampledata/sample_chanlocs_mapping_result.json'
+        )
+    modelname = 'ART'
+        
 
     # read the mapping result
-    mapping_name = './data/mapped_output.json'
-    mapping_result, num_channel, num_group = utils.read_mapping_result(mapping_name)
+    mapping_result, num_channel, num_group = utils.read_mapping_result(ic.mapping_result)
 
     for i in range(num_group):
 
         # step1: Data preprocessing
-        preprocess_data = utils.preprocessing(input_path+input_name, sample_rate, mapping_result[i])
+        preprocess_data = utils.preprocessing(ic.input_path+ic.input_name, ic.sample_rate, mapping_result[i])
         # step2: Signal reconstruction
-        reconstructed_data = utils.reconstruct(modelname, preprocess_data, output_name, i)
+        reconstructed_data = utils.reconstruct(modelname, preprocess_data, ic.output_name, i)
         # step3: Data postprocessing
-        utils.postprocessing(reconstructed_data, sample_rate, output_path+output_name, mapping_result[i], i, num_channel)
+        utils.postprocessing(reconstructed_data, ic.sample_rate, ic.output_path+ic.output_name, mapping_result[i], i, num_channel)
